44cfec95e78c3c9e1157f7ff59f4c79343af0b1f
CyberTech <cybertech@gmail.com>
2014-08-26 23:11:59 -0700

    Locally disable warning C4127: conditional expression is constant  (inside the DATATYPE macro)

--- MQ2Main/DataTypeList.h
+++ MQ2Main/DataTypeList.h
@@ -6,6 +6,10 @@
 #error __FILE__ included without defining DATATYPE(_class_, _var_, _inherits_, _persistentclass_)
 #endif
 
+#pragma warning( push )
+#pragma warning( disable:4127 ) // warning C4127: conditional expression is constant  (inside the DATATYPE macro)
+
+
 #ifndef ISXEQ
 // These datatypes are specific to MQ2 only
 DATATYPE(MQ2ArrayType,  pArrayType,  nullptr, 0);
@@ -68,5 +72,7 @@ DATATYPE(MQ2TimerType,            pTimerType,            nullptr, 0);
 DATATYPE(MQ2WindowType,           pWindowType,           nullptr, 0);
 DATATYPE(MQ2XTargetType,          pXTargetType,          pSpawnType, 0);
 DATATYPE(MQ2ZoneType,             pZoneType,             nullptr, 0);
+
+#pragma warning( pop )
 
 #undef DATATYPE // In case the includer forgets
